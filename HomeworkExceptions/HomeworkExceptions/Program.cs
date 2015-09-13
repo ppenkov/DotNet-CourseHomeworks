8175e3dac38df7627a1d28744f126c78be23ac36
ppenkov <penkov_law@abv.bg>
2015-09-13 15:04:53 +0300

    Homework Exceptions

---
+++ HomeworkExceptions/HomeworkExceptions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadFile("Text.txt");
+
+
+            Console.WriteLine("Enter the name of the file:");
+            string name = Console.ReadLine() + ".txt";
+            Console.WriteLine("Enter a word:");
+            string word = Console.ReadLine();
+            WriteFile(name, word);
+        }
+
+        static void ReadFile(string filename)
+        {
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                string line = reader.ReadToEnd();
+                Console.WriteLine(line);
+            }
+        }
+
+        static void WriteFile(string filename2, string word)
+        {
+            using (StreamWriter writer = new StreamWriter(filename2))
+            {
+                writer.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ HomeworkExceptions/Task2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                FinishLine();
+            }
+            catch (SentenceNotCompletedException)
+            {
+                Console.WriteLine("The text is not correctly written. Check the punctuation!");
+            }
+
+            finally
+            {
+                Console.WriteLine("Exception handling passed!");
+            }
+        }
+
+        static void FinishLine()
+        {
+            using (StreamReader text = new StreamReader("Text.txt"))
+            {
+                int lineNumber = 0;
+                bool symbol = false;
+                string line = text.ReadLine();
+
+                while (line != null)
+                {
+                    lineNumber++;
+
+                    if (line.LastIndexOf('.') == line.Length - 1)
+                    {
+                        symbol = true;
+                        line = text.ReadLine();
+                        continue;
+                    }
+                    else if (line.LastIndexOf('?') == line.Length - 1)
+                    {
+                        symbol = true;
+                        line = text.ReadLine();
+                        continue;
+                    }
+                    else if (line.LastIndexOf('!') == line.Length - 1)
+                    {
+                        symbol = true;
+                        line = text.ReadLine();
+                        continue;
+                    }
+                    else
+                    {
+                        symbol = false;
+                        break;
+                    }
+                }
+
+                if (symbol == true)
+                {
+                    Console.WriteLine("The punctuation of the text is correct!");
+                }
+                else
+                {
+                    throw new SentenceNotCompletedException();
+                }
+            }
+        }
+    }
+}
