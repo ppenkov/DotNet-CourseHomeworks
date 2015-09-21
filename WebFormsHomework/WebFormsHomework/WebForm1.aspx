2f9be502b53d421ff754a874bd219e6dee9bb8b9
ppenkov <penkov_law@abv.bg>
2015-09-21 18:39:33 +0300

    Homework

---
+++ WebFormsHomework/WebFormsHomework/WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebFormsHomework.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="label1" runat="server" Text="User Name: "/>
+            <asp:TextBox ID="TextBox1" runat="server"/><br/>
+            <asp:Label ID="label2" runat="server" Text="Password: "/>
+            <asp:TextBox ID="TextBox2" runat="server" TextMode="Password"/><br/>
+            <asp:Label ID="label3" runat="server" Text="Repeat Password: "/>
+            <asp:TextBox ID="TextBox3" runat="server" TextMode="Password"/><br/>
+            <asp:Label ID="label4" runat="server" Text="Age: "/>
+            <asp:TextBox ID="TextBox4" runat="server" /><br/>
+            <asp:Label ID="label5" runat="server" Text="E-mail: "/>
+            <asp:TextBox ID="TextBox5" runat="server"/><br/>
+            <asp:Label ID="label6" runat="server"/><br/>
+            <asp:Button ID="Button" runat="server" Text="Register" OnClick="Button_Click"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebFormsHomework/WebFormsHomework/WebForm1.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormsHomework
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            if ((TextBox5.Text).Contains('@') == false || (TextBox5.Text).Contains('.') == false)
+            {
+                label6.Text = "Please enter a correct e-mail!";
+            }
+            else
+            {
+                label6.Text = "";
+            }
+
+            using (StreamWriter writer = new StreamWriter(Server.MapPath("WriteLines.txt"), true))
+            {
+                writer.WriteLine(label1.Text + " " + TextBox1.Text);
+                writer.WriteLine(label2.Text + " " + TextBox2.Text);
+                writer.WriteLine(label3.Text + " " + TextBox3.Text);
+                writer.WriteLine(label4.Text + " " + TextBox4.Text);
+                writer.WriteLine(label5.Text + " " + TextBox5.Text);
+            }
+        }
+    }
+}
